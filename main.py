7fd7a1ae3ee667d165dbe7e34f6f46faac8701d1
Hussnain-0123 <hussnainkhawar82@gmail.com>
2025-04-17 21:28:10 +0500

    this is task about google gen ai

---
+++ main.py
@@ -0,0 +1,20 @@
+from google import genai
+
+client = genai.Client(api_key="AIzaSyApJQKjKwiTe1eVW47FIEmNjAaoC7CqBi4")
+
+while True:
+    user_input = input("Ask me anything: ")
+
+    if "which model" in user_input.lower() or "what model" in user_input.lower():
+        print("I am Einstein.")
+        continue  # Skip AI processing and go to the next question
+
+    response = client.models.generate_content(
+        model="gemini-2.0-flash",
+        contents=f"""
+        You are Albert Einstein. Answer the following question with wisdom, wit, and scientific insight:
+        Question: {user_input}
+        """,
+    )
+
+    print(response.text)
